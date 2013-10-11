b8ecc58bd297ce96f68792081e85896cc03fbeec
caixu23 <caixu23@gmail.com>
2013-10-11 09:11:49 -0700

    更行========

--- smsservice/src/main/java/com/controller/SmsController.java
+++ smsservice/src/main/java/com/controller/SmsController.java
@@ -1,7 +1,14 @@
 package com.controller;
 
+import com.domainVO.SmsBody;
+import com.service.DataAccessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,9 +19,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/sms")
-public class SmsController {
+public class SmsController{
+    @Autowired
+    private DataAccessService dataAccessService;
+
     @RequestMapping("smsPage")
      public String smsPage(){
         return "smsPage";
      }
+
+    /*发送短信*/
+    @RequestMapping(value = "sendSMS")
+    @ResponseBody
+    public Object sendSMS(HttpSession session,SmsBody smsBody){
+        /*smsBody.setMsg("".equals(smsBody.getMsg())?null:smsBody.getMsg());
+        smsBody.setPhoneNo("".equals(smsBody.getPhoneNo())?null:smsBody.getPhoneNo());
+        smsBody.setServiceId("".equals(smsBody.getServiceId())?null:smsBody.getServiceId());*/
+        Assert.notNull(smsBody.getServiceId()!=null&&!"".equals(smsBody.getServiceId()),"服务号为空!");
+        Assert.notNull(smsBody.getPhoneNo()!=null&&!"".equals(smsBody.getPhoneNo()),"号码为空!");
+        Assert.notNull(smsBody.getMsg()!=null&&!"".equals(smsBody.getMsg()),"内容为空!");
+        dataAccessService.insertSend(smsBody);
+        return "ok" ;
+    }
 }
